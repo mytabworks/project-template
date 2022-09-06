2bb5179aaf894434a0b935c3f0c0f8315be0a5b3
jr <fernando.tabamo@whitecobalt.com>
2022-09-06 13:16:21 +0800

    set up auth middleware

---
+++ server/middleware/Auth.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
+import BaseMiddleware from "./BaseMiddleware"
+
+export type Session = (ReturnType<typeof getSession> extends Promise<infer R> ? R : any) & { 
+    roles: number[];
+    uid: number;
+}
+
+export class Auth extends BaseMiddleware {
+
+    public static async handle(request: NextApiRequest, response: NextApiResponse<any>, authorizeRoleIDs: number[] | null): Promise<boolean> {
+
+        const session = await getSession({ req: request }) as Session
+        
+        if(!session 
+            || !(session 
+                && Array.isArray(authorizeRoleIDs) 
+                && session.roles.some((id) => authorizeRoleIDs.includes(id))
+            )
+        ) {
+
+            this.unauthorized(response)
+
+            return false
+        }
+
+        return true
+    }
+
+}
