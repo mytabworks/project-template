67dcdb3fe3e0b280ffff4583dedfe9a60dfc990d
jr <fernando.tabamo@whitecobalt.com>
2022-09-08 11:39:27 +0800

    fix unauthorize when has session but no roleIDs presented

--- server/middleware/Auth.ts
+++ server/middleware/Auth.ts
@@ -2,23 +2,24 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { getSession } from "next-auth/react"
 import BaseMiddleware from "./BaseMiddleware"
 
-export type Session = (ReturnType<typeof getSession> extends Promise<infer R> ? R : any) & { 
-    roles: number[];
-    uid: number;
-}
+export type Session = (ReturnType<typeof getSession> extends Promise<infer R> ? R : any)
 
 export class Auth extends BaseMiddleware {
 
     public static async handle(request: NextApiRequest, response: NextApiResponse<any>, authorizeRoleIDs: number[] | null): Promise<boolean> {
 
         const session = await getSession({ req: request }) as Session
-        
+
         if(!session 
             || !(session 
-                && Array.isArray(authorizeRoleIDs) 
-                && authorizeRoleIDs.length > 0
-                && session.roles.some((id) => authorizeRoleIDs.includes(id))
-            )
+                && (
+                    (Array.isArray(authorizeRoleIDs) 
+                        && authorizeRoleIDs.length > 0
+                        && session.user?.roles.some((id) => authorizeRoleIDs.includes(id)
+                    ) 
+                    || authorizeRoleIDs === null
+                )
+            ))
         ) {
 
             this.unauthorized(response)
