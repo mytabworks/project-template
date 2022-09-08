10eb12b71981b42a7a57b6ab2af38608197d4084
jr <fernando.tabamo@whitecobalt.com>
2022-09-08 11:36:54 +0800

    add registration and change password stuff

--- server/controller/User.ts
+++ server/controller/User.ts
@@ -3,6 +3,8 @@ import { ConnectionPool } from "eloquents"
 import { NextApiRequest, NextApiResponse } from "next"
 import Validozer from "validozer/esm"
 import BaseController from "./BaseController"
+import { compare, hash } from 'bcryptjs'
+import { getSession } from "next-auth/react"
 
 class UserController extends BaseController {
     
@@ -209,7 +211,7 @@ class UserController extends BaseController {
 
         try {
             await connectionPool.open()
-            await User.findAndDelete(id, ['activities', 'roles'])
+            await User.findAndDelete(id, ['activities', 'roles', 'providers'])
 
             return response.status(200).json({
                 success: true,
@@ -227,6 +229,162 @@ class UserController extends BaseController {
 
             connectionPool.close()
             
+        }
+    }
+
+    public static async registration(request: NextApiRequest, response: NextApiResponse<any>) {
+
+        const data = request.body
+        const rules = {
+            name: {
+                label: "Name",
+                rules: "required",
+            },
+            email: {
+                label: "E-mail",
+                rules: "required|email",
+            },
+            password: {
+                label: "Password",
+                rules: "required|min:8|max:20"
+            },
+            confirm: {
+                label: "Comfirm Password",
+                rules: "required|same:password@Password"
+            }
+        }
+
+        const validator = Validozer.make(data, rules)
+
+        if(validator.fails()) {
+            const errors = validator.errorsJSON()
+
+            return response.status(200).send({
+                success: false,
+                errors
+            })
+        }
+
+        const connectionPool = new ConnectionPool()
+
+        try {
+
+            await connectionPool.open()
+
+            const exist = await User.where('email', data.email).first()
+
+            if(exist.hasItem) {
+                return response.status(200).send({
+                    success: false,
+                    errors:[{
+                        field: 'email',
+                        message: "E-mail was already taken"
+                    }]
+                })
+            }
+
+            const user = new User()
+            user.name = data.name
+            user.email = data.email
+            user.password = await hash(data.password, 12)
+            await user.save()
+
+            // role 3 is consumer
+            const newRole = await user.roles().findOrNew(3)
+            await newRole.save()
+
+            return response.status(201).send({
+                success: true,
+                id: user.id,
+                message: `User registered successfully`
+            })
+        } catch (error) {
+            return response.status(500).send({
+                success: false,
+                message: error.message
+            })
+        } finally {
+
+            await connectionPool.close()
+        }
+    }
+
+    public static async changePassword(request: NextApiRequest, response: NextApiResponse<any>) {
+        const data = request.body
+        const rules = {
+            current: {
+                label: "Current Password",
+                rules: "required|min:8|max:20"
+            },
+            password: {
+                label: "Password",
+                rules: "required|min:8|max:20"
+            },
+            confirm: {
+                label: "Comfirm Password",
+                rules: "required|same:password@Password"
+            }
+        }
+
+        const validator = Validozer.make(data, rules)
+
+        if(validator.fails()) {
+            const errors = validator.errorsJSON()
+
+            return response.status(200).send({
+                success: false,
+                errors
+            })
+        }
+
+        const connectionPool = new ConnectionPool()
+
+        try {
+            await connectionPool.open()
+
+            const session = await getSession({ req: request })
+
+            const user = await User.find(session?.user.id!)
+
+            if(!user.hasItem) {
+
+                return response.status(200).send({
+                    success: false,
+                    message: "User don't exist"
+                })
+
+            }
+
+            const valid = await compare(data.password, user.password!)
+
+            if(!valid) {
+
+                return response.status(200).send({
+                    success: false,
+                    errors: [{
+                        field: 'current',
+                        message: "Current Password was incorrect"
+                    }]
+                })
+                
+            }
+
+            user.password = await hash(data.password, 12)
+
+            await user.save()
+
+            return response.status(200).send({
+                success: true,
+                message: 'User change password successfully'
+            })
+        } catch(error) {
+            return response.status(500).send({
+                success: false,
+                message: error.message
+            });
+        } finally {
+
+            await connectionPool.close()
         }
     }
 
