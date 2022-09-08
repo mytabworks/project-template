c4773f060a149288d90a3598dde184fdcb1ec5c7
jr <fernando.tabamo@whitecobalt.com>
2022-09-08 11:35:34 +0800

    add google auth & change session data

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
 import Route from "@route"
 import User from "@model/User"
+import UserProvider from "@model/UserProvider"
+import { ConnectionPool } from "eloquents"
+import { compare, hash } from "bcryptjs"
 
 const maxAge = 30 * 24 * 60 * 60 // 30 days
 
@@ -44,16 +48,22 @@ export const nextAuthOptions: NextAuthOptions = {
             }
 
             if(token.sub) {
-                const dbUser = await User.find(token.sub, ['id'], {
+                const dbUser = await User.find(token.sub, ['*'], {
                     roles: ($query) => {
                         $query.select('id')
                     }
                 })
 
                 session = {
-                    uid: dbUser.id,
-                    roles: dbUser.$roles?.map((role) => role.id),
                     ...session,
+                    user: {
+                        id: dbUser.id,
+                        profile_img: dbUser.profile_img,
+                        name: dbUser.name,
+                        email: dbUser.email,
+                        email_verified: dbUser.email_verified,
+                        roles: dbUser.$roles?.map((role) => role.id),
+                    } as any,
                     accessToken: token.accessToken  
                 }
             }
@@ -83,25 +93,132 @@ export const nextAuthOptions: NextAuthOptions = {
                 }
             },
             async authorize(credentials, _request) {
-            // You need to provide your own logic here that takes the credentials
-            // submitted and returns either a object representing a user or value
-            // that is false/null if the credentials are invalid.
-            // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-            // You can also use the `req` object to obtain additional parameters
-            // (i.e., the request IP address)
-            const user = await User
-                .select('id', 'name', 'email')
-                .where('email', credentials?.username)
-                .where('password', credentials?.password)
-                .first()
-        
-            // If no error and we have user data, return it
-            if (user.hasItem) {
-                return user.toJSON()
+                // You need to provide your own logic here that takes the credentials
+                // submitted and returns either a object representing a user or value
+                // that is false/null if the credentials are invalid.
+                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+                // You can also use the `req` object to obtain additional parameters
+                // (i.e., the request IP address)
+                const user = await User
+                    .select('id', 'name', 'email', 'password')
+                    .where('email', credentials?.username)
+                    .where('email_verified', true)
+                    .first()
+                    
+                    // If no error and we have user data, return it
+                if (user.hasItem) {
+                    
+                    const valid = await compare(credentials?.password || '', user.password || '')
+
+                    if(valid) {
+
+                        return {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email
+                        }
+
+                    } else {
+
+                        return null
+                    }
+                }
+                // Return null if user data could not be retrieved
+                return null
             }
-            // Return null if user data could not be retrieved
-            return null
-            }
+        }),
+        GoogleProvider({
+
+            clientId: process.env.OAUTH_GOOGLE_ID!,
+            clientSecret: process.env.OAUTH_GOOGLE_SECRET!,
+
+            async profile(profile): Promise<any> {
+
+                const connectionPool = new ConnectionPool()
+
+                try {
+                    await connectionPool.open()
+
+                    const provider = await UserProvider
+                        .where('uid', profile.sub)
+                        .where('type', 'google')
+                        .with('user')
+                        .first()
+
+                    if(provider.hasItem === false) {
+
+                        const user = await User
+                            .select('id')
+                            .where('email', profile.email)
+                            .first()
+
+                        if(user.hasItem === true) {
+                            
+                            const newProvider = user.providers().create()
+                            newProvider.type = 'google'
+                            newProvider.uid = profile.sub
+                            await newProvider.save()
+
+                            if(user.email_verified === false) {
+
+                                user.email_verified = true
+                                await user.save()
+                                
+                            }
+
+                            return {
+                                id: user.id,
+                                email: profile.email,
+                                name: profile.name
+                            }
+
+                        } else {
+
+                            const newUser = new User()
+                            newUser.name = profile.name
+                            newUser.email = profile.email
+                            newUser.profile_img = profile.picture
+                            newUser.email_verified = true
+                            await newUser.save()
+
+                            // role 3 is consumer
+                            const newRole = await newUser.roles().findOrNew(3)
+                            await newRole.save()
+
+                            const newProvider = newUser.providers().create()
+                            newProvider.type = 'google'
+                            newProvider.uid = profile.sub
+                            await newProvider.save()
+
+                            return {
+                                id: newUser.id,
+                                email: profile.email,
+                                name: profile.name
+                            }
+                        }
+
+                    } else {
+
+                        return {
+                            id: provider.user_id,
+                            email: provider.$user?.email,
+                            name: provider.$user?.name
+                            // Return all the profile information you need.
+                            // The only truly required field is `id`
+                            // to be able identify the account when added to a database
+                        }
+
+                    }
+
+                } catch (error) {
+
+                    console.error(error)
+                } finally {
+
+                    await connectionPool.close()
+                }
+                
+            },
         })
     ]
 }
--- server/model/User.ts
+++ server/model/User.ts
@@ -3,6 +3,7 @@ import { User as UserEntity } from '@entity/User'
 import Activity from './Activity'
 import Role from './Role'
 import UserRole from './UserRole'
+import UserProvider from './UserProvider'
 
 @ModelEntity(UserEntity, 'user')
 class User extends Model {
@@ -11,12 +12,15 @@ class User extends Model {
         'email',
         'name',
         'password',
+        'profile_img',
+        'email_verified'
     ]
 
     protected updatable = [
-        'email',
         'name',
         'password',
+        'profile_img',
+        'email_verified',
     ]
 
     public activities() {
@@ -25,6 +29,10 @@ class User extends Model {
 
     public roles() {
         return this.belongsToMany(Role, UserRole)
+    }
+
+    public providers() {
+        return this.hasMany(UserProvider)
     }
 }
 
