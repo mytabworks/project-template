29ed73bfeb2aa42a560ebfa696a4a7b7149b9850
jr <fernando.tabamo@whitecobalt.com>
2022-09-08 11:37:12 +0800

    change type

--- server/entity/User.ts
+++ server/entity/User.ts
@@ -6,14 +6,26 @@ export class User {
     @PrimaryGeneratedColumn()
     id!: number;
 
-    @Column()
+    @Column({type: 'varchar', length: 230, unique: true})
     email!: string;
 
-    @Column()
+    @Column({type: 'varchar', length: 230})
     name!: string;
 
-    @Column()
+    @Column('varchar', {
+        nullable: true
+    })
     password!: string;
+
+    @Column('varchar', {
+        nullable: true
+    })
+    profile_img!: string;
+
+    @Column('boolean', {
+        default: false
+    })
+    email_verified!: boolean;
 
     @Column('bigint')
     created_at!: number;
