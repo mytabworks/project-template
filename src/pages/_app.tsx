82901d8e1e0b4262c7d6e290e2a9ae0947a85cc3
jr <fernando.tabamo@whitecobalt.com>
2022-09-14 00:56:37 +0800

    check if user interacting for Web Push to ignore & for last online

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import Layout from '@client/components/custom/Layout'
+import UserInteractionChecker from '@client/components/UserInteractionChecker'
 import '@client/assets/styles/bootstrap-theme.scss'
 import '@client/assets/styles/globals.css'
 import '@client/common/utils/nextPWAUpdate'
@@ -9,9 +10,11 @@ function Application({ Component, pageProps: {session, ...pageProps} }: AppProps
 	
 	return (
 		<SessionProvider session={session}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<UserInteractionChecker>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</UserInteractionChecker>
 		</SessionProvider>
 	)
 }
