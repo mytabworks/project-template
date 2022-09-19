34ebc73d620175200e84037b738f07e38520488a
jr <fernando.tabamo@whitecobalt.com>
2022-09-19 19:50:00 +0800

    basic chat socket.io

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,27 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import Layout from '@client/components/custom/Layout'
 import UserInteractionChecker from '@client/components/UserInteractionChecker'
+import SocketClient from '@client/components/SocketClient';
+import SSRProvider from 'react-bootstrap/SSRProvider'
+import 'retoast/dist/css/main.css'
 import '@client/assets/styles/bootstrap-theme.scss'
 import '@client/assets/styles/globals.css'
-import '@client/common/utils/nextPWAUpdate'
+import '@client/pwa-update'
 
 function Application({ Component, pageProps: {session, ...pageProps} }: AppProps) {
 	
 	return (
-		<SessionProvider session={session}>
-			<UserInteractionChecker>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</UserInteractionChecker>
-		</SessionProvider>
+		<SSRProvider>
+			<SessionProvider session={session}>
+				<SocketClient>
+					<UserInteractionChecker>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</UserInteractionChecker>
+				</SocketClient>
+			</SessionProvider>
+		</SSRProvider>
 	)
 }
 
---
+++ src/pages/chat.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import { Button, Container, Spinner } from 'react-bootstrap'
+import ClientMiddleware from '@client/middleware'
+import { useAPI } from '@client/common/hooks/useAPI'
+import { RoleType } from '@server/model/Role'
+import Chatbox from '@client/components/Chatbox'
+
+
+const Chat: NextPage = (props) => {
+
+	return (
+		<Container>
+			<Chatbox />
+		</Container>
+	)
+}
+
+export const getServerSideProps = ClientMiddleware.roles([RoleType.ADMIN, RoleType.CLIENT]).redirects({ [RoleType.CLIENT]: '/customer' }).auth()
+
+export default Chat
