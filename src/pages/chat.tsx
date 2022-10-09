615ae2c8f1f6138a7b9d17d940f8c07017c0ddb0
jr <fernando.tabamo@whitecobalt.com>
2022-10-09 11:09:36 +0800

    update chatbox feature

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -3,7 +3,7 @@ import { Button, Container, Spinner } from 'react-bootstrap'
 import ClientMiddleware from '@client/middleware'
 import { useAPI } from '@client/common/hooks/useAPI'
 import { RoleType } from '@server/model/Role'
-import Chatbox from '@client/components/Chatbox'
+import Chatbox from '@client/components/custom/Chatbox'
 
 
 const Chat: NextPage = (props) => {
